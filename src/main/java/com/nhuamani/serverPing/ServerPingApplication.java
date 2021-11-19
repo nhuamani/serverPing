4f00afd7892e57dadad427cf9466792ad29a56cf
nhuamani <nhuamani.c7@gmail.com>
2021-11-18 22:56:03 -0500

    Start Project

---
+++ src/main/java/com/nhuamani/serverPing/ServerPingApplication.java
@@ -0,0 +1,13 @@
+package com.nhuamani.serverPing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhuamani/serverPing/ServerPingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhuamani.serverPing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
