bb344d3e4d6dbca9b1928927f78c14d8b9892e06
nhuamani <nhuamani.c7@gmail.com>
2021-11-19 22:43:44 -0500

    Mapear con JPA nuestra Entity(Model) Model y Enumeration, add Validation

---
+++ src/main/java/com/nhuamani/serverPing/models/Server.java
@@ -0,0 +1,27 @@
+package com.nhuamani.serverPing.models;
+
+import com.nhuamani.serverPing.enums.Status;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Server {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty or null")
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
