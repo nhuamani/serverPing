41ffc3fdd58f9924c7fc31f905d27a7adb158899
nhuamani <nhuamani.c7@gmail.com>
2021-11-20 16:33:26 -0500

    Agregando datos a Server Model por CommandLineRunner

--- src/main/java/com/nhuamani/serverPing/ServerPingApplication.java
+++ src/main/java/com/nhuamani/serverPing/ServerPingApplication.java
@@ -3,12 +3,28 @@ package com.nhuamani.serverPing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+import com.nhuamani.serverPing.enums.Status;
+import com.nhuamani.serverPing.models.Server;
+import com.nhuamani.serverPing.repository.ServerRepository;
+
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class ServerPingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerPingApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16GB", "Personal PC","http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower","http://localhost:8080/server/image/server2.png", Status.SERVER_DOWN));
+			serverRepository.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/server4.png", Status.SERVER_DOWN));
+		};
 	}
 
 }
